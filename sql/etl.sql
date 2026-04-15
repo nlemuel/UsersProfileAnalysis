f7976815890b15a77b322e53e374778125404ec9
nlemuel <lemuelayres@gmail.com>
2026-04-15 11:18:39 -0300

    feat(sql): add solution for question 3

--- sql/etl.sql
+++ sql/etl.sql
@@ -6,16 +6,35 @@ WITH tb_transacoes AS (
         datetime(substr(dtCriacao, 1, 19)) AS dtCriacao,
         julianday('now') - julianday(substr(dtCriacao, 1, 19)) AS diffDate
     FROM transacoes
+),
+
+tb_cliente AS (
+    SELECT 
+        IdCliente,
+        QtdePontos,
+        datetime(substr(dtCriacao, 1, 19)) AS dtCriacao,
+        cast(julianday('now') - julianday(substr(dtCriacao, 1, 19)) AS integer) AS idadeBase
+    FROM clientes
+),
+
+tb_sumario_transacoes AS (
+    SELECT 
+        IdCliente,
+        count(IdTransacao) AS TransacoesVida,
+        count(CASE WHEN diffDate <= 56 THEN IdTransacao END) AS Transacoes56,
+        count(CASE WHEN diffDate <= 28 THEN IdTransacao END) AS Transacoes28,
+        count(CASE WHEN diffDate <= 14 THEN IdTransacao END) AS Transacoes14,
+        count(CASE WHEN diffDate <= 7 THEN IdTransacao END) AS Transacoes7,
+        
+        cast(min(diffDate) AS INTEGER) AS diasUltimaInteracao
+
+    FROM tb_transacoes
+    GROUP BY IdCliente
 )
 
 SELECT 
-    IdCliente,
-    count(IdTransacao) AS TransacoesVida,
-    count(CASE WHEN diffDate <= 56 THEN IdTransacao END) AS Transacoes56,
-    count(CASE WHEN diffDate <= 28 THEN IdTransacao END) AS Transacoes28,
-    count(CASE WHEN diffDate <= 14 THEN IdTransacao END) AS Transacoes14,
-    count(CASE WHEN diffDate <= 7 THEN IdTransacao END) AS Transacoes7,
-    cast(min(diffDate) AS INTEGER) AS diasUltimaTransacao
-
-FROM tb_transacoes
-GROUP BY IdCliente
+    t1.*,
+    t2.idadeBase
+FROM tb_sumario_transacoes AS t1
+LEFT JOIN tb_cliente AS t2
+ON t1.idCLiente = t2.idCliente
